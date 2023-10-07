46802727b0092fe60fc9081c701ee8cd220fc653
Kachirou <hoang286272@gmail.com>
2023-10-07 17:28:35 +0700

    up

--- app/src/main/java/com/neatroots/instagram/MainActivity.kt
+++ app/src/main/java/com/neatroots/instagram/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.neatroots.instagram
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.statusBarColor= Color.TRANSPARENT
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,SiginUpActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
