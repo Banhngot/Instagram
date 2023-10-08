a556ab35988f829f57549b53b28a2600dc1590a0
Kachirou <hoang286272@gmail.com>
2023-10-08 18:04:55 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.neatroots.instagram"
     compileSdk = 33
-
+    buildFeatures{
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "com.neatroots.instagram"
         minSdk = 26
@@ -41,11 +44,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.8.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
 
 }
--- app/src/main/java/com/neatroots/instagram/SiginUpActivity.kt
+++ app/src/main/java/com/neatroots/instagram/SiginUpActivity.kt
@@ -2,10 +2,48 @@ package com.neatroots.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.neatroots.instagram.Models.User
+import com.neatroots.instagram.databinding.ActivitySiginUpBinding
 
 class SiginUpActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySiginUpBinding.inflate(layoutInflater)
+    }
+    lateinit var user: User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sigin_up)
+        setContentView(binding.root)
+        user = User()
+        binding.signUpBtn.setOnClickListener{
+            if(binding.name.editText?.text.toString().equals("") or
+                binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")
+                ){
+                Toast.makeText(this@SiginUpActivity, "Please fill all the Information",Toast.LENGTH_SHORT).show()
+            }else{
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                ).addOnCompleteListener{
+                    result->
+
+                    if(result.isSuccessful){
+                        user.name=binding.name.editText?.text.toString()
+                        user.password=binding.password.editText?.text.toString()
+                        user.email=binding.email.editText?.text.toString()
+                        Firebase.firestore.collection("User").document(Firebase.auth.currentUser!!.uid).set(user).addOnSuccessListener {
+                            Toast.makeText(this@SiginUpActivity,"Login",Toast.LENGTH_SHORT).show()
+                        }
+                    }else{
+                        Toast.makeText(this@SiginUpActivity,result.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
