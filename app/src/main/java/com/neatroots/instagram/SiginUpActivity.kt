7b2385927d23bd4886d5d91945620df7f8c78ccd
Kachirou <hoang286272@gmail.com>
2023-10-08 18:36:32 +0700

    up

--- app/src/main/java/com/neatroots/instagram/SiginUpActivity.kt
+++ app/src/main/java/com/neatroots/instagram/SiginUpActivity.kt
@@ -3,11 +3,15 @@ package com.neatroots.instagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.neatroots.instagram.Models.User
+import com.neatroots.instagram.Ultils.USER_NODE
+import com.neatroots.instagram.Ultils.USER_PROFILE_FOLDER
+import com.neatroots.instagram.Ultils.uploadImage
 import com.neatroots.instagram.databinding.ActivitySiginUpBinding
 
 class SiginUpActivity : AppCompatActivity() {
@@ -15,6 +19,19 @@ class SiginUpActivity : AppCompatActivity() {
         ActivitySiginUpBinding.inflate(layoutInflater)
     }
     lateinit var user: User
+    private val launcher= registerForActivityResult(ActivityResultContracts.GetContent()){
+        uri->
+        uri?.let {
+            uploadImage(uri, USER_PROFILE_FOLDER){
+                if(it==null){
+
+                }else{
+                    user.image=it
+                    binding.profileImage.setImageURI(uri)
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -36,7 +53,7 @@ class SiginUpActivity : AppCompatActivity() {
                         user.name=binding.name.editText?.text.toString()
                         user.password=binding.password.editText?.text.toString()
                         user.email=binding.email.editText?.text.toString()
-                        Firebase.firestore.collection("User").document(Firebase.auth.currentUser!!.uid).set(user).addOnSuccessListener {
+                        Firebase.firestore.collection(USER_NODE).document(Firebase.auth.currentUser!!.uid).set(user).addOnSuccessListener {
                             Toast.makeText(this@SiginUpActivity,"Login",Toast.LENGTH_SHORT).show()
                         }
                     }else{
@@ -44,6 +61,9 @@ class SiginUpActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+        binding.addImage.setOnClickListener{
+            launcher.launch("image/*")
         }
     }
 }
---
+++ app/src/main/java/com/neatroots/instagram/Ultils/Constrain.kt
@@ -0,0 +1,4 @@
+package com.neatroots.instagram.Ultils
+
+const val USER_NODE= "User"
+const val USER_PROFILE_FOLDER = "Profile"
