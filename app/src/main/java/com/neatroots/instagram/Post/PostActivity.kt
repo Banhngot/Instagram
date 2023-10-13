73e37819dbe34392f4055662f7e3b75fbf013243
Kachirou <hoang286272@gmail.com>
2023-10-13 09:23:46 +0700

    up

--- app/src/main/java/com/neatroots/instagram/Post/PostActivity.kt
+++ app/src/main/java/com/neatroots/instagram/Post/PostActivity.kt
@@ -2,6 +2,15 @@ package com.neatroots.instagram.Post
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.neatroots.instagram.Models.Post
+import com.neatroots.instagram.Ultils.POST
+import com.neatroots.instagram.Ultils.POST_FOLDER
+import com.neatroots.instagram.Ultils.USER_PROFILE_FOLDER
+import com.neatroots.instagram.Ultils.uploadImage
 
 
 import com.neatroots.instagram.databinding.ActivityPostBinding
@@ -9,6 +18,21 @@ import com.neatroots.instagram.databinding.ActivityPostBinding
 class PostActivity : AppCompatActivity() {
     val binding by lazy {
         ActivityPostBinding.inflate(layoutInflater)
+    }
+    var imageUrl:String?=null
+    private val launcher= registerForActivityResult(ActivityResultContracts.GetContent()){
+            uri->
+        uri?.let {
+            uploadImage(uri, POST_FOLDER){
+                url ->
+                if(url!=null){
+                     binding.selectImage.setImageURI(uri)
+
+                    imageUrl=url
+
+                }
+            }
+        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +43,22 @@ class PostActivity : AppCompatActivity() {
         getSupportActionBar()?.setDisplayShowHomeEnabled(true)
         binding.materialToolbar.setNavigationOnClickListener {
             finish()
+        }
+
+
+        binding.selectImage.setOnClickListener{
+            launcher.launch("image/*")
+        }
+
+        binding.postButton.setOnClickListener {
+            val post:Post= Post(imageUrl!!,binding.caption.editText?.text.toString())
+
+            Firebase.firestore.collection(POST).document().set(post).addOnSuccessListener {
+                Firebase.firestore.collection(Firebase.auth.currentUser!!.uid).document().set(post).addOnSuccessListener {
+                    finish()
+                }
+
+            }
         }
     }
 }
