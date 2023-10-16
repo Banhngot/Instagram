63418638bfa8978111474a4d3611a7b98730f23f
Kachirou <hoang286272@gmail.com>
2023-10-16 20:02:46 +0700

    up

--- app/src/main/java/com/neatroots/instagram/Fragments/MyPostFragment.kt
+++ app/src/main/java/com/neatroots/instagram/Fragments/MyPostFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.neatroots.instagram.Adapers.MyPostRvAdapter
+import com.neatroots.instagram.Models.Post
 import com.neatroots.instagram.R
 import com.neatroots.instagram.databinding.FragmentMyPostBinding
 
@@ -34,7 +41,19 @@ class MyPostFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding=FragmentMyPostBinding.inflate(inflater, container, false)
-
+        var postList=ArrayList<Post>()
+        var adapter=MyPostRvAdapter(requireContext(),postList)
+//        binding.rv.layoutManager=StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL)
+//        binding.rv.Adapter=adapter
+        Firebase.firestore.collection(Firebase.auth.currentUser!!.uid).get().addOnSuccessListener {
+            var tempList= arrayListOf<Post>()
+            for (i in it.documents){
+                var post:Post=i.toObject<Post>()!!
+                tempList.add(post)
+            }
+            postList.addAll(tempList)
+            adapter.notifyDataSetChanged()
+        }
         return binding.root
     }
 
